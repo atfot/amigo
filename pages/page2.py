b5983b350a77f38ee48984ef5b9e0f135f66bead
atfot <104991246+atfot@users.noreply.github.com>
2024-02-05 22:43:06 +0900

    chatbot

--- pages/page2.py
+++ pages/page2.py
@@ -1,17 +1,41 @@
-from navigation import make_sidebar
+from openai import OpenAI
 import streamlit as st
+from navigation import make_sidebar
 
 make_sidebar()
+user_name='현규씨'
 
-st.write(
-    """
-# Chatbot Area
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-This is a secret page that only logged-in users can see.
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-Don't tell anyone.
-
-For real.
-
-"""
+if prompt := st.chat_input():
+    client = OpenAI(api_key='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')
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt) #user message
+    response = client.chat.completions.create(
+  model="gpt-3.5-turbo-16k",
+  messages=[
+    {
+      "role": "system",
+      "content": "You are a helpful korean counselor that serves single korean person. you should reply like an actual human."
+    },
+    {
+      "role": "user",
+      "content": f"""
+      user-name : {user_name}
+      Question : {st.session_state.messages}
+      """
+    }
+  ],
+  temperature=1,
+  max_tokens=512,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
 )
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg) #bot response
