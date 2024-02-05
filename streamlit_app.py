491287b5870eded087fe2cbc42e8715ddb962581
atfot <104991246+atfot@users.noreply.github.com>
2024-02-05 20:11:46 +0900

    login_2

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,20 @@ st.write("Please login (username `test`, password `test`).")
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
-if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+col1, col2, col3 = st.columns([2,6,2])
+with col1:
+    if st.button("Log in", type="primary"):
+        if username == "test" and password == "test":
+            st.session_state.logged_in = True
+            st.success("Logged in successfully!")
+            sleep(0.5)
+            st.switch_page("pages/page1.py")
+        else:
+            st.error("Incorrect username or password")
+with col3:
+    if st.button("New User", type="secondary"):
         st.session_state.logged_in = True
-        st.success("Logged in successfully!")
-        sleep(0.5)
-        st.switch_page("pages/page1.py")
-    else:
-        st.error("Incorrect username or password")
+            st.success("Welcome to Amigo!")
+            sleep(0.5)
+            st.switch_page("pages/page2.py")
+        
