272bec4d00d165855cd00e2fda30eb88efe51e37
atfot <104991246+atfot@users.noreply.github.com>
2024-02-05 20:04:30 +0900

    login_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 from time import sleep
-from navigation import make_sidebar
 
-make_sidebar()
+st.title("Amigo")
 
-st.title("Welcome to Diamond Corp")
-
-st.write("Please log in to continue (username `test`, password `test`).")
+st.write("Please login (username `test`, password `test`).")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
