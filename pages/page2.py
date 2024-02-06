77b6a1c4415ca9f472775d00e7b14246278df203
atfot <104991246+atfot@users.noreply.github.com>
2024-02-06 16:21:04 +0900

    df

--- pages/page2.py
+++ pages/page2.py
@@ -5,7 +5,7 @@ from navigation import make_sidebar
 make_sidebar()
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "I'm here to listen to your mental problems. Can you tell me yours?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Welcome back. Can you tell me your mental problems?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
