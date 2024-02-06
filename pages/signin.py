db18e488ccded4a1c6a5db85b9506bf596b3be32
atfot <104991246+atfot@users.noreply.github.com>
2024-02-06 12:20:26 +0900

    df

--- pages/signin.py
+++ pages/signin.py
@@ -6,9 +6,10 @@ st.write(
 # Okay! Nice to meet you sir. 
 # Please fill in the blanks for sign up.
 
-Jess hyung this part is signin form. But I haven't finished yet.
+Jess hyung this part is signin form. 
+But I haven't finished yet.
 I was thinking which database to save my user's info for this - any big company's SQL Server, or just Google Spreadsheets.
-and this page needs some buttons to perfect it.
+And this page needs some buttons to perfect it.
 """
 )
 x=0
