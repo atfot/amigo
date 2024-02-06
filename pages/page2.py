112e1ffe0b069a6b3a678e80a4bd979d58592f22
atfot <104991246+atfot@users.noreply.github.com>
2024-02-06 12:19:16 +0900

    df

--- pages/page2.py
+++ pages/page2.py
@@ -3,9 +3,9 @@ import streamlit as st
 from navigation import make_sidebar
 
 make_sidebar()
-user_name='Dita'
-user_gender='Female'
-age='27'
+user_name='Jess'
+user_gender='Male'
+age='29'
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Got any worries on your mind? Tell us all about it."}]
