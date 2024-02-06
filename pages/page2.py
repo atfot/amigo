d644ade41e2636f52371b5f6731be96e0230b1f6
atfot <104991246+atfot@users.noreply.github.com>
2024-02-06 18:15:21 +0900

    sfd

--- pages/page2.py
+++ pages/page2.py
@@ -149,7 +149,7 @@ Your goal is to provide in-depth, expert, and accurate analysis and opinions acr
 )
       msg = response.choices[0].message.content
       start = msg.find("**Friendly Translation**: ") + len("**Friendly Translation**: ")
-      new_msg = msg[start:]
+      new_msg = 'Final Output: ' + msg[start:]
       st.session_state.messages.append({"role": "assistant", "content": new_msg})
       st.chat_message("assistant").write(msg)
       st.chat_message("assistant").write(new_msg)
