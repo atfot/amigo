a9651ac36a0d82127742c9246024877fae0df206
atfot <104991246+atfot@users.noreply.github.com>
2024-02-06 12:19:55 +0900

    dfdf

--- pages/signin.py
+++ pages/signin.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 st.write(
     """
-Okay! Nice to meet you sir. Please fill in the blanks for sign up.
+# Okay! Nice to meet you sir. 
+# Please fill in the blanks for sign up.
 
-# Jess hyung this part is signin form. But I haven't finished yet.
-# I was thinking which database to save my user's info for this - any big company's SQL Server, or just Google Spreadsheets.
-# and this page needs some buttons to perfect it.
+Jess hyung this part is signin form. But I haven't finished yet.
+I was thinking which database to save my user's info for this - any big company's SQL Server, or just Google Spreadsheets.
+and this page needs some buttons to perfect it.
 """
 )
 x=0
