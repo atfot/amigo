1c4e944c94a62c32be4c475fe9cdb80c44df1193
atfot <104991246+atfot@users.noreply.github.com>
2024-02-07 20:02:35 +0900

    sf

--- pages/page3.py
+++ pages/page3.py
@@ -9,7 +9,7 @@ if "messages" not in st.session_state:
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-    st.write(st.session_state.messages[1])
+st.write(st.session_state.messages)
 
 if prompt := st.chat_input():
     client = OpenAI(api_key=st.secrets['api_key'])
