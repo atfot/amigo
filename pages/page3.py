5fe6723ba510cee1d8f81660e74666d12a4fc09b
atfot <104991246+atfot@users.noreply.github.com>
2024-03-20 22:48:01 +0900

    1

--- pages/page3.py
+++ pages/page3.py
@@ -130,11 +130,11 @@ if prompt := st.chat_input():
         "content": f"{st.session_state.conversations}"
       }
     ],
-    temperature=1.1,
+    temperature=1,
     max_tokens=1024,
-    top_p=1,
-    frequency_penalty=0.6,
-    presence_penalty=1.1
+    top_p=0.9,
+    frequency_penalty=1.1,
+    presence_penalty=1.2
   )
       time.sleep(0.1)
       msg = response.choices[0].message.content
