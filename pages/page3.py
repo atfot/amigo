bbd4f6e144626828c21b02d20c211ef791109121
atfot <104991246+atfot@users.noreply.github.com>
2024-03-20 21:38:06 +0900

    test

--- pages/page3.py
+++ pages/page3.py
@@ -134,7 +134,7 @@ if prompt := st.chat_input():
     max_tokens=1024,
     top_p=1,
     frequency_penalty=1,
-    presence_penalty=1
+    presence_penalty=0
   )
       msg = response.choices[0].message.content
       new_msg = msg[msg.find("**Best response**:") + len("**Best response**:"):msg.find("**Why")].strip().strip('"')
