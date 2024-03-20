1445ecd44b78a67687dbb8b003f6ab1c120a11d8
atfot <104991246+atfot@users.noreply.github.com>
2024-03-20 18:52:28 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,18 @@
 import streamlit as st
 from time import sleep
 
-if 'korean_langauge' not in st.session_state:
+language_selection=st.selectbox(
+            'Language(언어)',
+            ('한국어','English')
+        )
+if language_selection=='English': 
     col1,col2,col3=st.columns([4,2,4])
     with col2:
         st.title("Neri")
     st.title('')
     col1,col2=st.columns([6,4])
-    with col1:
+    with col2:
         st.write("Please login (username `test`, password `test`).")
-    with col2:
-        language_selection=st.selectbox(
-            'Language(언어)',
-            ('한국어','English')
-        )
-if 'korean_langauge' in st.session_state:
-    col1,col2,col3=st.columns([4,2,4])
-    with col2:
-        st.title("네리")
-    st.title('')
-    col1,col2=st.columns([6,4])
-    with col1:
-        st.write("로그인해주세요 (유저이름 `test`, 패스워드 `test`).")
-    with col2:
-        language_selection=st.selectbox(
-            'Language(언어)',
-            ('한국어','English')
-        )
-if language_selection=='English': 
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
@@ -65,7 +50,13 @@ if language_selection=='English':
         pass
 
 if language_selection=='한국어': 
-    st.session_state['korean_language']=True
+    col1,col2,col3=st.columns([4,2,4])
+    with col2:
+        st.title("네리")
+    st.title('')
+    col1,col2=st.columns([6,4])
+    with col2:
+        st.write("로그인해주세요 (유저이름 `test`, 패스워드 `test`).")
     username = st.text_input("유저 이름")
     password = st.text_input("비밀번호", type="password")
 
