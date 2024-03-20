d9f2d3feea9a0d85be7af75384d2977df4bc79a5
atfot <104991246+atfot@users.noreply.github.com>
2024-03-20 17:40:03 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,32 @@
 import streamlit as st
 from time import sleep
 
-col1,col2,col3=st.columns([4,2,4])
-with col2:
-    st.title("Neri")
-st.title('')
-col1,col2=st.columns([6,4])
-with col1:
-    st.write("Please login (username `test`, password `test`).")
-with col2:
-    language_selection=st.selectbox(
-        'Language(언어)',
-        ('Korean','English')
-    )
+if 'korean_langauge' not in st.session_state:
+    col1,col2,col3=st.columns([4,2,4])
+    with col2:
+        st.title("Neri")
+    st.title('')
+    col1,col2=st.columns([6,4])
+    with col1:
+        st.write("Please login (username `test`, password `test`).")
+    with col2:
+        language_selection=st.selectbox(
+            'Language(언어)',
+            ('한국어','English')
+        )
+if 'korean_langauge' in st.session_state:
+    col1,col2,col3=st.columns([4,2,4])
+    with col2:
+        st.title("네리")
+    st.title('')
+    col1,col2=st.columns([6,4])
+    with col1:
+        st.write("로그인해주세요 (유저이름 `test`, 패스워드 `test`).")
+    with col2:
+        language_selection=st.selectbox(
+            'Language(언어)',
+            ('한국어','English')
+        )
 if language_selection=='English': 
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
@@ -20,7 +34,7 @@ if language_selection=='English':
     col1, col2 = st.columns([8.5,1.5])
     with col1:
         if st.button("New User", type="secondary"):
-            st.session_state.logged_in = True
+            st.session_state.signin = True
             st.success("Welcome to Neri!")
             sleep(0.5)
             st.switch_page("pages/signin.py")
@@ -33,16 +47,29 @@ if language_selection=='English':
                 st.switch_page("pages/page1.py")
             else:
                 st.error("Incorrect username or password")
+    if st.session_state.logged_in == True:
+        col, col2, col3 = st.columns([3,4,3])
+        with col2:
+            st.success("Logged in successfully!")
+            sleep(0.5)
+            st.switch_page("pages/page1.py")
+    if st.session_state.signin == True:
+        col, col2, col3 = st.columns([3,4,3])
+        with col2:
+            st.success("Welcome to Neri!")
+            sleep(0.5)
+            st.switch_page("pages/signin.py")
 
 if language_selection=='Korean': 
+    st.session_state['Korean_language']=True
     username = st.text_input("유저 이름")
     password = st.text_input("비밀번호", type="password")
 
     col1, col2 = st.columns([8.7,1.3])
     with col1:
         if st.button("새로 오신 분", type="secondary"):
-            st.session_state.logged_in = True
-            st.success("네리에 오신 것을 환영합니다!")
+            st.session_state.signin = True
+            st.success("성공적으로 로그인 되었습니다!")
             sleep(0.5)
             st.switch_page("pages/signin.py")
     with col2:
@@ -54,4 +81,15 @@ if language_selection=='Korean':
                 st.switch_page("pages/page1.py")
             else:
                 st.error("유저 이름 또는 패스워드가 맞지 않습니다.")
-            
+    if st.session_state.logged_in == True:
+        col, col2, col3 = st.columns([3,4,3])
+        with col2:
+            st.success("성공적으로 로그인 되었습니다!")
+            sleep(0.5)
+            st.switch_page("pages/page1.py")
+    if st.session_state.signin == True:
+        col, col2, col3 = st.columns([3,4,3])
+        with col2:
+            st.success("성공적으로 로그인 되었습니다!")
+            sleep(0.5)
+            st.switch_page("pages/signin.py")
