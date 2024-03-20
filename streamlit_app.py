85d8237b60e95e078e3cd8edc2439970a6b3009c
atfot <104991246+atfot@users.noreply.github.com>
2024-03-20 17:28:50 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if language_selection=='English':
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
-    col1, col2 = st.columns([1.5,8.5])
+    col1, col2 = st.columns([8.5,1.5])
     with col1:
         if st.button("New User", type="secondary"):
             st.session_state.logged_in = True
@@ -38,7 +38,7 @@ if language_selection=='Korean':
     username = st.text_input("유저 이름")
     password = st.text_input("비밀번호", type="password")
 
-    col1, col2 = st.columns([1.8,8.2])
+    col1, col2 = st.columns([8.2,1.8])
     with col1:
         if st.button("새로 오신 분", type="secondary"):
             st.session_state.logged_in = True
