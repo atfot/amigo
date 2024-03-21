e2140133bca216c5f520857afa664423fa9293cf
atfot <104991246+atfot@users.noreply.github.com>
2024-03-21 22:43:08 +0900

    d

--- pages/page3.py
+++ pages/page3.py
@@ -99,8 +99,9 @@ if prompt := st.chat_input():
           - Please read this conversation carefully and respond in the form below.
           **REMEMBER**: Use this form below. **DO NOT USE LINE BREAKS OR SPACES** that are not depicted in the form below.
           '''
-          **What to know before you write**: 
-          [Please write down the entire contents of "# Things to know before writing" here]
+          **Background of the conversation**: [{st.session_state.message_summary}]
+
+          **Conversation content**: [{st.session_state.conversations}]
 
           **Three psychotherapist's responses**: 
           [Given the above conversation, what are the 3 correct responses from the psychotherapist?]
