a9e629e7a1420fb98b7dd1c5b0ac4e8f48a1a770
atfot <104991246+atfot@users.noreply.github.com>
2024-03-21 21:57:37 +0900

    dz

--- pages/page3.py
+++ pages/page3.py
@@ -125,9 +125,9 @@ if prompt := st.chat_input():
         "content": f"{user_prompt}"
       }
     ],
-    temperature=0.1,
+    temperature=1,
     max_tokens=15500,
-    top_p=1,
+    top_p=0.1,
     frequency_penalty=1,
     presence_penalty=1
   )
