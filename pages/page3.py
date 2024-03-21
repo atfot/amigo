bcce6bd4bbd5f12c459134ab5aab24557d9aa207
atfot <104991246+atfot@users.noreply.github.com>
2024-03-21 23:10:21 +0900

    sf

--- pages/page3.py
+++ pages/page3.py
@@ -122,11 +122,11 @@ if prompt := st.chat_input():
         "content": f"{user_prompt}"
       }
     ],
-    temperature=0.1,
+    temperature=1,
     max_tokens=1024,
-    top_p=0.1,
-    frequency_penalty=0,
-    presence_penalty=0
+    top_p=0.9,
+    frequency_penalty=1,
+    presence_penalty=1
   )
       time.sleep(1)
       msg = response.choices[0].message.content
@@ -147,11 +147,11 @@ Please only show the sentences from the '**Best response**:' section of what I p
 """
       }
     ],
-    temperature=0.09,
+    temperature=0.1,
     max_tokens=2048,
-    top_p=1,
-    frequency_penalty=1,
-    presence_penalty=1
+    top_p=0.1,
+    frequency_penalty=0,
+    presence_penalty=0
   )
       
       new_msg = response.choices[0].message.content.strip('"')
