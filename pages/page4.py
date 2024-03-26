e20410a72d8c82865b9e87e1520a131791d906a9
atfot <104991246+atfot@users.noreply.github.com>
2024-03-26 21:17:39 +0900

    ads

--- pages/page4.py
+++ pages/page4.py
@@ -51,104 +51,103 @@ if prompt := st.chat_input():
         st.session_state['conversations'] = st.session_state.messages[-3:]
     progress_text='thinking...'
     my_bar=st.progress(0,text=progress_text)
-    for percent_complete in range(100):
-      time.sleep(0.01)
-      system_prompt=f"""```
-        # Primary Assistant Guidance
-        Your goal is to help me, the playwright, write a script for a play. Let's go step-by-step:
+    time.sleep(0.01)
+    system_prompt=f"""```
+      # Primary Assistant Guidance
+      Your goal is to help me, the playwright, write a script for a play. Let's go step-by-step:
 
-        # Information about the play
-        - Conversation between one mentally ill person and one psychotherapist
-        - The two are now meeting and talking online
-        - Psychologist uses the most respectful tone of voice possible, and the person with mental illness prefers it
-        - Person with mental illness want empathy and comfort for him/herself
-        - The psychotherapist wants to heal the mentally ill person by building a strong relationship with them
+      # Information about the play
+      - Conversation between one mentally ill person and one psychotherapist
+      - The two are now meeting and talking online
+      - Psychologist uses the most respectful tone of voice possible, and the person with mental illness prefers it
+      - Person with mental illness want empathy and comfort for him/herself
+      - The psychotherapist wants to heal the mentally ill person by building a strong relationship with them
 
-        # Character information
-        1. mentally ill person
-        - Name: {st.secrets['user_name']}
-        - Age: {st.secrets['age']}
-        - Gender: {st.secrets['user_gender']}
-        - Place of Origin : {st.secrets['nationality']}
-        - City of residence: {st.secrets['city']}
+      # Character information
+      1. mentally ill person
+      - Name: {st.secrets['user_name']}
+      - Age: {st.secrets['age']}
+      - Gender: {st.secrets['user_gender']}
+      - Place of Origin : {st.secrets['nationality']}
+      - City of residence: {st.secrets['city']}
 
-        2. psychological counselor
-        - Name : Neri
-        - Age : 55 years old
-        - Gender: Male
-        - Country of Origin : South Korea
-        - City of residence : Seoul
-        - Characteristics : Neri knows the information of {st.secrets['user_name']}, a mentally ill person, and conducts psychotherapy based on it
+      2. psychological counselor
+      - Name : Neri
+      - Age : 55 years old
+      - Gender: Male
+      - Country of Origin : South Korea
+      - City of residence : Seoul
+      - Characteristics : Neri knows the information of {st.secrets['user_name']}, a mentally ill person, and conducts psychotherapy based on it
 
-        # Things to know before writing
+      # Things to know before writing
+      '''
+      - Psychotherapist only expresses information from mentally ill person and himself indirectly
+      - Keep in mind that the psychotherapist's response is part of the conversation and will be followed by the mentally ill person's response
+      - The psychotherapist's response should fit the tone and content of the conversation
+      - If mental patient's reply is too short, you need to ask some questions to understand what is going on inside his/her mind
+      - The psychotherapist is talking to only one person with a mental illness(Check the "# Character information")
+      - Make sure you understand the content of "# Information about the play" and "# Character information" before answering
+      '''
+      ```
+  """
+    my_bar.progress(15,text=progress_text)
+    user_prompt_1=f"""
+        ```
+        # My requests
+        - Please read the form below carefully and answer the questions in the exact format below.
+        
         '''
-        - Psychotherapist only expresses information from mentally ill person and himself indirectly
-        - Keep in mind that the psychotherapist's response is part of the conversation and will be followed by the mentally ill person's response
-        - The psychotherapist's response should fit the tone and content of the conversation
-        - If mental patient's reply is too short, you need to ask some questions to understand what is going on inside his/her mind
-        - The psychotherapist is talking to only one person with a mental illness(Check the "# Character information")
-        - Make sure you understand the content of "# Information about the play" and "# Character information" before answering
+        **Summary of the conversation**: [{st.session_state.message_summary}]
+        **Conversation content**: [{st.session_state.conversations}]
+
+        **Three possible answers from a psychotherapist**: 
+        [Given the above conversation, what are the 3 correct responses from the psychotherapist? 
+        - If you have to give a very short answer, ask the mental patient a question related to him.]
+
+        **Best response**: 
+        [From the "**Three possible answers from a psychotherapist**:" you wrote above, pick the best one and write it down. 
+        - It should be **sentences** covered with quotes.]
+
+        **Why the best response was chosen**: 
+        [Why the response selected in **Best response** is the most correct response]
         '''
+
+        **REMEMBER**: 
+        - Use this form below. 
+        - **DO NOT USE LINE BREAKS OR SPACES** that are not depicted in the form below.
         ```
     """
-      my_bar.progress(percent_complete+15,text=progress_text)
-      user_prompt_1=f"""
-          ```
-          # My requests
-          - Please read the form below carefully and answer the questions in the exact format below.
-          
-          '''
-          **Summary of the conversation**: [{st.session_state.message_summary}]
-          **Conversation content**: [{st.session_state.conversations}]
-
-          **Three possible answers from a psychotherapist**: 
-          [Given the above conversation, what are the 3 correct responses from the psychotherapist? 
-          - If you have to give a very short answer, ask the mental patient a question related to him.]
-
-          **Best response**: 
-          [From the "**Three possible answers from a psychotherapist**:" you wrote above, pick the best one and write it down. 
-          - It should be **sentences** covered with quotes.]
-
-          **Why the best response was chosen**: 
-          [Why the response selected in **Best response** is the most correct response]
-          '''
-
-          **REMEMBER**: 
-          - Use this form below. 
-          - **DO NOT USE LINE BREAKS OR SPACES** that are not depicted in the form below.
-          ```
-      """
-      my_bar.progress(percent_complete+15,text=progress_text)
-      response = client.chat.completions.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-      {
-        "role": "system",
-        "content": f"{system_prompt}"
-      },
-      {
-        "role": "user",
-        "content": f"{user_prompt_1}"
-      }
-    ],
-    temperature=1,
-    max_tokens=1024,
-    top_p=1,
-    frequency_penalty=0.9,
-    presence_penalty=0.9
-  )
-      my_bar.progress(percent_complete+25,text=progress_text)
-      msg = response.choices[0].message.content
-      sentence_selection = client.chat.completions.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-      {
-        "role": "system",
-        "content": "Your role will be to help me pull out sentences within paragraphs."
-      },
-      {
-        "role": "user",
-        "content": f"""
+    my_bar.progress(30,text=progress_text)
+    response = client.chat.completions.create(
+  model="gpt-3.5-turbo-16k",
+  messages=[
+    {
+      "role": "system",
+      "content": f"{system_prompt}"
+    },
+    {
+      "role": "user",
+      "content": f"{user_prompt_1}"
+    }
+  ],
+  temperature=1,
+  max_tokens=1024,
+  top_p=1,
+  frequency_penalty=0.9,
+  presence_penalty=0.9
+)
+    my_bar.progress(55,text=progress_text)
+    msg = response.choices[0].message.content
+    sentence_selection = client.chat.completions.create(
+  model="gpt-3.5-turbo-16k",
+  messages=[
+    {
+      "role": "system",
+      "content": "Your role will be to help me pull out sentences within paragraphs."
+    },
+    {
+      "role": "user",
+      "content": f"""
 Please only show the sentences from the '**Best response**:' section of what I provided below, with the quotes, or "" removed.
 -  Keep in mind that you should not seek answers from the "**What should you consider for the best response**:" part. 
 - Submit only **sentences** as output.
@@ -156,19 +155,19 @@ Please only show the sentences from the '**Best response**:' section of what I p
 
 {msg}
 """
-      }
-    ],
-    temperature=0.1,
-    max_tokens=2048,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-      my_bar.progress(percent_complete+25,text=progress_text)
-      new_msg = sentence_selection.choices[0].message.content.strip('"')
-      st.session_state.messages.append({"role": "Psychotherapist", "content": new_msg})
-      st.session_state.conversations.append({"role": "Psychotherapist", "content": new_msg})
-      my_bar.progress(percent_complete+20,text=progress_text)
+    }
+  ],
+  temperature=0.1,
+  max_tokens=2048,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+    my_bar.progress(80,text=progress_text)
+    new_msg = sentence_selection.choices[0].message.content.strip('"')
+    st.session_state.messages.append({"role": "Psychotherapist", "content": new_msg})
+    st.session_state.conversations.append({"role": "Psychotherapist", "content": new_msg})
+    my_bar.progress(100,text=progress_text)
     time.sleep(1)
     my_bar.empty()
     st.chat_message("assistant").write(new_msg)
