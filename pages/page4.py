4f6b46f7b18e4a89ac9e0df8938e6a228a7c2d51
atfot <104991246+atfot@users.noreply.github.com>
2024-03-26 16:51:30 +0900

    dz

--- pages/page4.py
+++ pages/page4.py
@@ -24,6 +24,7 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     client = OpenAI(api_key=st.secrets['api_key'])
     st.session_state.messages.append({"role": "Mental patient", "content": prompt})
+    st.session_state.conversations.append({"role": "Mental patient", "content": prompt})
     st.chat_message("user").write(prompt)
     if len(st.session_state.messages)<6:
        st.session_state['message_summary'] = 'Nothing has been written to date, and the conversation starts below.'
@@ -49,9 +50,6 @@ if prompt := st.chat_input():
         )
         st.session_state['message_summary'] = summary.choices[0].message.content
         st.session_state['conversations'] = st.session_state.messages[-3:]
-    if len(st.session_state.messages)>6:
-       if len(st.session_state.messages)%6!=0:
-          st.session_state.conversations.append({"role": "Mental patient", "content": prompt})
     with st.spinner('thinking...'):
       system_prompt=f"""```
         # Primary Assistant Guidance
@@ -160,9 +158,7 @@ Please only show the sentences from the '**Best response**:' section of what I p
       
       new_msg = sentence_selection.choices[0].message.content.strip('"')
       st.session_state.messages.append({"role": "Psychotherapist", "content": new_msg})
-      if len(st.session_state.messages)>6:
-       if len(st.session_state.messages)%6!=0:
-          st.session_state.conversations.append({"role": "Psychotherapist", "content": new_msg})
+      st.session_state.conversations.append({"role": "Psychotherapist", "content": new_msg})
       st.chat_message("assistant").write(new_msg)
       st.write(user_prompt_1)
       st.chat_message("assistant").write(msg)
