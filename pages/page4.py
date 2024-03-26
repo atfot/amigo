69e5fda3c29a7aaf84a5ab81aa4345c7c0f9b1fa
atfot <104991246+atfot@users.noreply.github.com>
2024-03-26 23:17:19 +0900

    ㅁㄴㅇ

--- pages/page4.py
+++ pages/page4.py
@@ -84,6 +84,7 @@ if prompt := st.chat_input():
       - Keep in mind that the psychotherapist's response is part of the conversation and will be followed by the mentally ill person's response
       - The psychotherapist's response should fit the tone and content of the conversation
       - If mental patient's reply is too short, you need to ask some questions to understand what is going on inside his/her mind
+      - If you get a very short answer from the mental patient, ask him/her a related question, but don't ask how he/she feel.
       - The psychotherapist is talking to only one person with a mental illness(Check the "# Character information")
       - Make sure you understand the content of "# Information about the play" and "# Character information" before answering
       '''
@@ -96,17 +97,17 @@ if prompt := st.chat_input():
         - Please read the form below carefully and answer the questions in the exact format below.
         
         '''
+        **What should I consider for the best answer**: 
+        [Write down what you need to consider for the best answer]
+
         **Summary of the conversation**: [{st.session_state.message_summary}]
         **Conversation content**: [{st.session_state.conversations}]
 
         **Three possible answers from a psychotherapist**: 
-        [Given the above conversation, what are the 3 correct responses from the psychotherapist? **If you get a very short answer from the mental patient, ask him/her a related question, but don't ask how he/she feel.**]
+        [Given the above conversation, what are the 3 correct responses from the psychotherapist? ]
 
         **Best response**: 
         [Pick the best one from the "**Three possible answers from a psychotherapist**:" and write it down. It should be **sentences** covered with quotes.]
-
-        **Why the best response was chosen**: 
-        [Why the response selected in **Best response** is the most correct response]
         '''
 
         **REMEMBER**: 
